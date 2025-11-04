3cebb66725a3127cfaaf432fe7b516d124eb72f2
ManolitoPiesDePlata <1913000@alu365.murciaeduca.es>
2025-11-04 13:46:41 +0100

    actualizamos todo el entorno cliente

---
+++ Entorno Cliente/crud/crud.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+let nombre = document.getElementById("nombre");
+let apellidos = document.getElementById("apellidos");
+let dni = document.getElementById("dni");
+let email = document.getElementById("email");
+
+
+});
